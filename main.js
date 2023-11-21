112343112a3b7cc0a1f644643768575322526dbc
Sagor Mohammed Yousuf <smyousuf630@gmail.com>
2023-11-21 18:40:36 +0600

    weather-app-with_Js

--- main.js
+++ main.js
@@ -6,40 +6,36 @@ const searchBtn = document.querySelector(".search-btn")
 const weatherIcon = document.querySelector(".weather-icon")
 
 
-async function checkWeather (city) {
+async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
 
-    
+
 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
-    document.querySelector(".humidity").innerHTML = data.main.humidity +"%";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 
 
     if (data.weather[0].main == "Clouds") {
         weatherIcon.src = "images/clouds.png";
-    }   else if (data.weather[0].main == "Clear") {
+    } else if (data.weather[0].main == "Clear") {
         weatherIcon.src = "images/clear.png";
     } else if (data.weather[0].main == "Rain") {
         weatherIcon.src = "images/clouds.png";
-    }else if (data.weather[0].main == "Drizzle") {
+    } else if (data.weather[0].main == "Drizzle") {
         weatherIcon.src = "images/drizzle.png";
     } else if (data.weather[0].main == "Mist") {
         weatherIcon.src = "images/Mist.png"
     }
-
-
-
-
 
 }
 
 
 
 
-searchBtn.addEventListener("click" , ()=> {
+searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value)
 })
 
